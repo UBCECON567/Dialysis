e088f7cda173e214a1001e941a1781d7adef55e2
Paul Schrimpf <paul.schrimpf@gmail.com>
2020-01-10 11:50:23 -0800

    Removed old notebook. Jupyter notebook can now be found in gh-pages branch.

--- notebooks/make.jl
+++
@@ -1,7 +0,0 @@
-using Weave
-weave("dialysis.jmd",out_path=:doc,
-      mod=Main,
-      doctype="pandoc2html",
-      pandoc_options=["--toc","--toc-depth=2","--filter=pandoc-citeproc"],
-      cache=:user) 
-notebook("dialysis.jmd", :pwd, -1, "--allow-errors")
